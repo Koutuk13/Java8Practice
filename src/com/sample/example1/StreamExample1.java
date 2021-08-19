7250edd9d5d6c3c7b4b810f775d285d3d784c3a0
Koutuk13 <koutuk90@gmail.com>
2021-08-19 10:10:50 +0530

    Initial commit: Stream Example

---
+++ src/com/sample/example1/StreamExample1.java
@@ -0,0 +1,22 @@
+package com.sample.example1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample1 {
+
+    public static void main(String[] args) {
+
+        List<String> listCountry = Arrays.asList("india","japan","singapore","usa","congo","aus");
+
+        listCountry.stream().filter(c-> c.length()>3).map(c-> c.toUpperCase()).forEach(System.out::println);
+
+        System.out.println("====================================");
+
+        List<Integer> numbers = Arrays.asList(100,500,600,200);
+        System.out.println(numbers.stream().reduce(300,(a,b)->a+b));
+
+        System.out.println("====================================");
+    }
+}
